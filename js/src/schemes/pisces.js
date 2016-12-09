73e11cbe63cefde7f5186fcbdf3a65454177e61c
keliet <kly-10@qq.com>
2016-12-09 19:28:49 +0800

    Site updated: 2016-12-09 19:28:48

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -14,4 +14,18 @@ $(document).ready(function () {
       setSidebarMarginTop(getSidebarTop());
     }
   });
+
+
+
+	$('.btn').hide();
+
+	$('.post-body').click(function(){
+		var h = $(this).find('.btn').attr('href');
+		window.location.href = h;
+	});
+
+
+	
+
+
 });
